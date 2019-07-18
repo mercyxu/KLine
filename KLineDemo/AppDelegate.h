30060fa8470b7ed5606d49bd2abd94b99a31395e
润果 <mercy.xu@qq.com>
2019-07-18 10:38:47 +0800

    Initial Commit

---
+++ KLineDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KLineDemo
+//
+//  Created by iOS on 2019/7/18.
+//  Copyright © 2019 www.KlineDemo.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KLineDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  KLineDemo
+//
+//  Created by iOS on 2019/7/18.
+//  Copyright © 2019 www.KlineDemo.com. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
